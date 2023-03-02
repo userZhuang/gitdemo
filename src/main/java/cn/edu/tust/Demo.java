edbd2463c724877245dc8ce0d25623cc6d66e140
zhuang <zhuang_ss@163.com>
2023-03-02 22:35:02 +0800

    third commit

--- src/main/java/cn/edu/tust/Demo.java
+++ src/main/java/cn/edu/tust/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args){
         System.out.println("hahah");
         System.out.println("haha2222");
+        System.out.println("haha3333");
     }
 }
